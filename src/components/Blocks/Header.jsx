91837b2b82e0d85ded0a4598aac36a9a55d3b40e
sadFrog-guy <adikokey@mail.ru>
2024-08-03 14:11:15 +0600

    fix of header

--- src/components/Blocks/Header.jsx
+++ src/components/Blocks/Header.jsx
@@ -1,11 +1,16 @@
 import logoMini from '../../assets/logo-mini.png'
 import {navItems} from '../../utils/consts';
+import useMediaQuery from '../hooks/useMediaQuery';
 import BurgerMenu from '../UI/BurgerMenu';
 import NavItem from '../UI/NavItem';
 
 export default function Header() {
+  const query = useMediaQuery("(min-width: 360px) and (max-width: 500px)")
+
   return (
-    <header className='w-full h-[12vh] sticky top-0 left-0 z-50 limit-x bg-white flex justify-between items-center header-mobile border-b-2 border-solid border-custom-orange'>
+    <header
+      className={`w-full h-[12vh] z-50 limit-x bg-white flex justify-between items-center header-mobile ${query ? 'border-b-2 border-solid border-custom-orange sticky top-0 left-0' : ''}`}
+    >
         <img src={logoMini} className='h-[80%]' alt="" />
 
         <div className="flex h-full hide-on-mobile">
